aa9c2cc18e7ccc2f086c8006417b1caf20de932b
Quentin Gosset <quentingosset7500@hotmail.com>
2019-10-01 18:58:03 +0200

    fix heroku

--- config/database.php
+++ config/database.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Str;
 $url  = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+@$host = $url["host"];
+@$username = $url["user"];
+@$password = $url["pass"];
+@$database = substr($url["path"], 1);
 
 return [
 
