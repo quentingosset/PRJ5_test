5fc4513c7d5013b2c06e6e27da365cfe32cf5866
Gosset Quentin <49923@etu.he2b.be>
2019-10-03 15:04:36 +0200

    test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,7 @@
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
     <h5 class="my-0 mr-md-auto font-weight-normal">
         @if( $page === 'listings' || $page === 'welcome')
-        Programme des présences :
+        Programme des présences 2 :
         @else
         @endif
     </h5>
